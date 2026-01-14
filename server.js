3a2fbc37417b6cb03620d781e41a3dd87ef8440b
Playmaker4339 <cesar-15210050@umb.edu.mx>
2026-01-14 09:38:03 -0600

    Actualizacion de server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = require('./server_sqlite');
 // En desarrollo local, usamos 3000 por defecto
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
+server.on('error', (err) => {
+  console.error('Error al iniciar el servidor:', err);
 });
